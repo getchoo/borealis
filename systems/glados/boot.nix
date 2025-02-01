7981773d75304e780e3e462b9888a9f6635960e0
Seth Flynn <getchoo@tuta.io>
2025-02-01 09:34:54 -0500

    glados: don't use gsp on proprietary driver

--- systems/glados/boot.nix
+++ systems/glados/boot.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   inputs,
@@ -19,7 +20,13 @@
 
     kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
 
-    kernelParams = [ "amd_pstate=active" ];
+    kernelParams =
+      [
+        "amd_pstate=active"
+      ]
+      # Don't use GSP Firmware on proprietary driver
+      # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/693
+      ++ lib.optional (!config.hardware.nvidia.open) "nvidia.NVreg_EnableGpuFirmware=0";
 
     loader.systemd-boot.enable = lib.mkForce false; # lanzaboote replaces this
 
