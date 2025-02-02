47be32eaba6ee39a09b3407c007300944677e1ff
Seth Flynn <getchoo@tuta.io>
2025-02-01 23:53:28 -0500

    shared/nix: use nix-community cachix

--- modules/shared/defaults/nix.nix
+++ modules/shared/defaults/nix.nix
@@ -26,6 +26,11 @@ in
             "flakes"
             "auto-allocate-uids"
           ];
+
+          extra-trusted-substituters = [ "https://nix-community.cachix.org" ];
+          extra-trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
         };
 
         gc = {
