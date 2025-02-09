52f7e42262adb0760a79a3e60d891b131d2c813f
Seth Flynn <getchoo@tuta.io>
2025-02-08 20:48:49 -0500

    nixos/acme: actually act as a mixin

--- modules/nixos/mixins/acme.nix
+++ modules/nixos/mixins/acme.nix
@@ -1,52 +1,13 @@
 {
-  config,
   lib,
-  secretsDir,
   ...
 }:
-let
-  cfg = config.mixins.acme;
-in
+
 {
-  options.mixins.acme = {
-    enable = lib.mkEnableOption "ACME mixin";
-
-    manageSecrets = lib.mkEnableOption "automatic management of secrets" // {
-      default = config.traits.secrets.enable;
-      defaultText = lib.literalExpression "config.traits.secrets.enable";
-    };
-
-    useDns = lib.mkEnableOption "the use of Cloudflare to obtain certs" // {
-      default = true;
+  security.acme = {
+    acceptTerms = lib.mkDefault true;
+    defaults = {
+      email = lib.mkDefault "getchoo@tuta.io";
     };
   };
-
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        security.acme = {
-          acceptTerms = true;
-          defaults = {
-            email = "getchoo@tuta.io";
-          };
-        };
-      }
-
-      (lib.mkIf cfg.useDns {
-        security.acme.defaults = {
-          dnsProvider = "cloudflare";
-        };
-      })
-
-      (lib.mkIf cfg.manageSecrets {
-        age.secrets = {
-          cloudflareApiKey.file = secretsDir + "/cloudflareApiKey.age";
-        };
-
-        security.acme.defaults = {
-          credentialsFile = config.age.secrets.cloudflareApiKey.path;
-        };
-      })
-    ]
-  );
 }
