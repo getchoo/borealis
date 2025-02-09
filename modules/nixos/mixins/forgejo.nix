c168f8427c86336f143b04c65a06a7d12aa62631
Seth Flynn <getchoo@tuta.io>
2025-02-08 22:30:48 -0500

    nixos/forgejo: actually act as a mixin

--- modules/nixos/mixins/forgejo.nix
+++ modules/nixos/mixins/forgejo.nix
@@ -1,47 +1,51 @@
 { config, lib, ... }:
 
 let
-  cfg = config.mixins.forgejo;
+  forgejoCfg = config.services.forgejo;
 in
 
 {
-  options.mixins.forgejo = {
-    enable = lib.mkEnableOption "default configuration for Forgejo";
-  };
-
-  config = lib.mkIf cfg.enable {
-    services.forgejo = {
-      enable = true;
-
-      database = {
-        type = "postgres";
-      };
-
-      settings = {
-        server = {
-          PROTOCOL = "http+unix";
-          DOMAIN = "git." + config.networking.domain;
-          ROOT_URL = "https://" + config.services.forgejo.settings.server.DOMAIN + "/";
-
-          DISABLE_SSH = true;
+  config = lib.mkMerge [
+    {
+      services.forgejo = {
+        database = {
+          type = "postgres";
         };
 
-        oauth2_client = {
-          ENABLE_AUTO_REGISTRATION = true;
-        };
+        settings = {
+          server = {
+            PROTOCOL = "http+unix";
+            DOMAIN = lib.mkDefault ("git." + config.networking.domain);
+            ROOT_URL = "https://" + forgejoCfg.settings.server.DOMAIN + "/";
 
-        service = {
-          DISABLE_REGISTRATION = lib.mkDefault true;
-        };
+            DISABLE_SSH = lib.mkDefault true;
+          };
 
-        packages = {
-          ENABLED = false;
-        };
+          oauth2_client = {
+            ENABLE_AUTO_REGISTRATION = lib.mkDefault true;
+          };
 
-        actions = {
-          ENABLED = false;
+          service = {
+            DISABLE_REGISTRATION = lib.mkDefault true;
+          };
+
+          packages = {
+            ENABLED = lib.mkDefault false;
+          };
+
+          actions = {
+            ENABLED = lib.mkDefault false;
+          };
         };
       };
-    };
-  };
+    }
+
+    (lib.mkIf forgejoCfg.enable {
+      services.nginx.virtualHosts.${forgejoCfg.settings.server.DOMAIN} = {
+        locations."/" = {
+          proxyPass = "http://unix:${forgejoCfg.settings.server.HTTP_ADDR}";
+        };
+      };
+    })
+  ];
 }
--- systems/atlas/forgejo.nix
+++ systems/atlas/forgejo.nix
@@ -1,17 +1,5 @@
-{ config, ... }:
-
-let
-  forgejoCfg = config.services.forgejo;
-in
-
 {
-  mixins.forgejo.enable = true;
-
-  services.nginx.virtualHosts = {
-    "git.getchoo.com" = {
-      locations."/" = {
-        proxyPass = "http://unix:${forgejoCfg.settings.server.HTTP_ADDR}";
-      };
-    };
+  services = {
+    forgejo.enable = true;
   };
 }
