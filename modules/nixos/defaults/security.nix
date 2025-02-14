c1bea770122a7cf2dea5113387265f59010d5a7f
Seth Flynn <getchoo@tuta.io>
2025-02-13 23:58:06 -0500

    modules/nixos: `sudo` -> `run0`

--- modules/nixos/defaults/security.nix
+++ modules/nixos/defaults/security.nix
@@ -1,12 +1,24 @@
-# Much of this is sourced from https://xeiaso.net/blog/paranoid-nixos-2021-07-18/
 { lib, ... }:
+
+# Much of this is sourced from https://xeiaso.net/blog/paranoid-nixos-2021-07-18/
 {
   security = {
     apparmor.enable = lib.mkDefault true;
     audit.enable = lib.mkDefault true;
     auditd.enable = lib.mkDefault true;
+
+    pam.services = {
+      # Fix `run0`
+      # TODO: Upstream?
+      systemd-run0 = {
+        startSession = true;
+        setEnvironment = true;
+      };
+    };
+
     polkit.enable = true;
-    sudo.execWheelOnly = true;
+
+    sudo.enable = false;
   };
 
   services.dbus.apparmor = lib.mkDefault "enabled";
