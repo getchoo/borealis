0f630c8671acd1b0c3a441e83fd49e3d84783439
Seth Flynn <getchoo@tuta.io>
2025-02-25 22:29:26 -0500

    nixos/nix: get NIX_PATH from system derivation

--- modules/nixos/defaults/nix.nix
+++ modules/nixos/defaults/nix.nix
@@ -1,12 +1,32 @@
-{ lib, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+
+let
+  flakeInputs = pkgs.linkFarm "flake-inputs" (
+    lib.mapAttrs (lib.const (flake: flake.outPath)) inputs
+  );
+in
+
 {
   nix = {
     channel.enable = lib.mkDefault false;
+
+    nixPath = lib.mkForce (
+      lib.mapAttrsToList (name: lib.const "${name}=/run/current-system/inputs/${name}") inputs
+    );
+
     settings.trusted-users = [
-      "root"
       "@wheel"
     ];
   };
 
   nixpkgs.config.allowAliases = false;
+
+  system.extraSystemBuilderCmds = ''
+    ln -s ${flakeInputs} $out/inputs
+  '';
 }
