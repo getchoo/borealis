468d8d1476830c074a792cf7429703c477b5c7bd
Seth Flynn <getchoo@tuta.io>
2025-02-27 01:16:36 -0500

    flake: use lix HEAD
    For fun! And since I'm compiling it anyways
    
    • Added input 'lix':
        'https://git.lix.systems/api/v1/repos/lix-project/lix/archive/8082f0b8434a81db309ac8abf1cf32532ba9168e.tar.gz?narHash=sha256-i6fadzFVD7B8XWXa2ycPoVlpSJ5SeWcZ8LLzfzYedKs%3D&rev=8082f0b8434a81db309ac8abf1cf32532ba9168e' (2025-02-27)
    • Updated input 'lix-module':
        'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/b90bf629bbd835e61f1317b99e12f8c831017006.tar.gz?narHash=sha256-YMLrcBpf0TR5r/eaqm8lxzFPap2TxCor0ZGcK3a7%2Bb8%3D' (2025-01-18)
      → 'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/621aae0f3cceaffa6d73a4fb0f89c08d338d729e.tar.gz?narHash=sha256-NG3IRvRs3u3btVCN861FqHvgOwqcNT/Oy6PBG86F5/E%3D&rev=621aae0f3cceaffa6d73a4fb0f89c08d338d729e' (2025-01-29)
    • Updated input 'lix-module/lix':
        'https://git.lix.systems/api/v1/repos/lix-project/lix/archive/2837da71ec1588c1187d2e554719b15904a46c8b.tar.gz?narHash=sha256-CCKIAE84dzkrnlxJCKFyffAxP3yfsOAbdvydUGqq24g%3D&rev=2837da71ec1588c1187d2e554719b15904a46c8b' (2025-01-18)
      → follows 'lix'

--- flake.nix
+++ flake.nix
@@ -110,10 +110,16 @@
       };
     };
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/refs/tags/2.92.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs = {
         nixpkgs.follows = "nixpkgs";
+        lix.follows = "lix";
         flakey-profile.follows = "";
       };
     };
