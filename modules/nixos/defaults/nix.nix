a2a6f14223905fd6963e5a6e424ac1366ec220e6
Seth Flynn <getchoo@tuta.io>
2025-03-07 18:41:30 -0500

    modules/nix: try to work around flake copying issues

--- modules/nixos/defaults/nix.nix
+++ modules/nixos/defaults/nix.nix
@@ -1,23 +1,8 @@
-{
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
-
-let
-  flakeInputs = pkgs.linkFarm "flake-inputs" (
-    lib.mapAttrs (lib.const (flake: flake.outPath)) inputs
-  );
-in
+{ lib, ... }:
 
 {
   nix = {
     channel.enable = lib.mkDefault false;
-
-    nixPath = lib.mkForce (
-      lib.mapAttrsToList (name: lib.const "${name}=/run/current-system/inputs/${name}") inputs
-    );
 
     settings.trusted-users = [
       "@wheel"
@@ -25,8 +10,4 @@ in
   };
 
   nixpkgs.config.allowAliases = false;
-
-  system.extraSystemBuilderCmds = ''
-    ln -s ${flakeInputs} $out/inputs
-  '';
 }
