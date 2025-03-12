e1291aaf52ed6355af78e69bfae21907ee9979d4
Seth Flynn <getchoo@tuta.io>
2025-03-11 23:00:46 -0400

    modules/nix: enable `use-xdg-base-directories`

--- modules/shared/defaults/nix.nix
+++ modules/shared/defaults/nix.nix
@@ -21,6 +21,7 @@ in
       nix = {
         settings = {
           auto-optimise-store = lib.mkDefault isLinux;
+
           experimental-features = [
             "nix-command"
             "flakes"
@@ -36,6 +37,8 @@ in
             "getchoo.cachix.org-1:ftdbAUJVNaFonM0obRGgR5+nUmdLMM+AOvDOSx0z5tE="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
+
+          use-xdg-base-directories = true;
         };
 
         gc = {
