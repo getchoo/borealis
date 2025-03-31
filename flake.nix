1d2c93163b6fb039dafbf53020d18d12595ff461
Seth Flynn <getchoo@tuta.io>
2025-03-30 22:15:19 -0400

    flake: update getchvim & nixpkgs-tracker-bot
    Flake lock file updates:
    
    • Updated input 'getchvim':
        'github:getchoo/getchvim/22b376b1bb1739e58c06e2bb120d9c0f4c3fbc9a' (2025-03-22)
      → 'github:getchoo/getchvim/fb01fd764b73372bdf91b9fb1ae2d02e469f0216' (2025-03-31)
    • Removed input 'getchvim/nix-filter'
    • Updated input 'nixpkgs-tracker-bot':
        'github:getchoo/nixpkgs-tracker-bot/c2b5fdf271170f1fd8b65dc15ee94173afc382a4' (2025-03-24)
      → 'github:getchoo/nixpkgs-tracker-bot/f13e56b26633e9da9896da36a75becc850480bc8' (2025-03-31)
    • Removed input 'nixpkgs-tracker-bot/nix-filter'

--- flake.nix
+++ flake.nix
@@ -138,7 +138,6 @@
       url = "github:getchoo/nixpkgs-tracker-bot";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nix-filter.follows = "getchvim/nix-filter";
         treefmt-nix.follows = "";
       };
     };
