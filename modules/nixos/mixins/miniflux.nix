41b52c30cf3c6304e28727430deee37106ebb0ef
Seth Flynn <getchoo@tuta.io>
2025-04-09 15:44:59 -0400

    nixos/miniflux: use unix socket

--- modules/nixos/mixins/miniflux.nix
+++ modules/nixos/mixins/miniflux.nix
@@ -9,24 +9,22 @@
     services = {
       nginx.virtualHosts.${lib.removePrefix "https://" config.services.miniflux.config.BASE_URL} = {
         locations."/" = {
-          proxyPass = "http://${config.services.miniflux.config.LISTEN_ADDR}";
+          proxyPass = "http://unix:${lib.head config.systemd.sockets.miniflux.listenStreams}";
         };
       };
     };
 
-    /*
-        # Create the socket manually to ensure NGINX has permission for the socket's parent directory
-        # ...since for some reason Miniflux will not give it the same `0777` permission as the socket itself
-        systemd = {
-          services.miniflux = {
-            requires = [ "miniflux.socket" ];
-          };
-
-        sockets.miniflux = {
-          wantedBy = [ "sockets.target" ];
-          listenStreams = [ "/run/miniflux.sock" ];
-        };
+    # Create the socket manually to ensure NGINX has permission for the socket's parent directory
+    # ...since for some reason Miniflux will not give it the same `0777` permission as the socket itself
+    systemd = {
+      services.miniflux = {
+        requires = [ "miniflux.socket" ];
       };
-    */
+
+      sockets.miniflux = {
+        wantedBy = [ "sockets.target" ];
+        listenStreams = [ "/run/miniflux.sock" ];
+      };
+    };
   };
 }
