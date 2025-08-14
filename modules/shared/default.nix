03ce5a980cc6c21f01b0e6ea7b65420386c34d8d
Seth Flynn <getchoo@tuta.io>
2025-08-14 16:18:23 -0400

    modules: set time zone and configuration rev again
    Accidentally deleted this in 5ff977f

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,18 @@
+{ lib, inputs, ... }:
+
+let
+  inherit (inputs) self;
+in
+
 {
   imports = [
     ./custom
     ./mixins
     ./users
   ];
+
+  system.configurationRevision = self.rev or self.dirtyRev or "dirty-unknown";
+
+  # TODO: Maybe don't set this globally?
+  time.timeZone = lib.mkDefault "America/New_York";
 }
