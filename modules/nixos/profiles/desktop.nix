bc5471eacaddbf868aa51e4a3b63ff3957c7e942
Seth Flynn <getchoo@tuta.io>
2025-08-20 13:20:38 -0400

    nixos/desktop: pin widevine to available version
    Currently the latest versions 404s

--- modules/nixos/profiles/desktop.nix
+++ modules/nixos/profiles/desktop.nix
@@ -22,7 +22,24 @@ in
     ];
 
     nixpkgs.overlays = lib.mkIf config.services.xserver.enable [
-      (final: prev: { chromium = prev.chromium.override { enableWideVine = final.config.allowUnfree; }; })
+      (final: prev: {
+        chromium = prev.chromium.override {
+          enableWideVine = final.config.allowUnfree;
+        };
+
+        # TODO: Unpin this when the latest version doesn't 404, lol
+        widevine-cdm = prev.widevine-cdm.overrideAttrs (
+          finalAttrs: _: {
+            version = "4.10.2830.0";
+
+            src = final.fetchzip {
+              url = "https://dl.google.com/widevine-cdm/${finalAttrs.version}-linux-x64.zip";
+              hash = "sha256-XDnsan1ulnIK87Owedb2s9XWLzk1K2viGGQe9LN/kcE=";
+              stripRoot = false;
+            };
+          }
+        );
+      })
     ];
 
     programs = {
