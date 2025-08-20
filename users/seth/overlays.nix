355f607407136efd80cfadb2b61cb19c575fad28
Seth Flynn <getchoo@tuta.io>
2025-08-20 02:08:27 -0400

    seth/getchvim: force final nixComponents on nixd overlay
    This shouldn't be needed, but hey, easyOverlay

--- users/seth/overlays.nix
+++ users/seth/overlays.nix
@@ -1,4 +1,20 @@
 inputs: [
   inputs.getchvim.overlays.default
   inputs.nixd.overlays.default
+
+  # Make sure nixd actually uses nix stuff from local `pkgs`
+  (
+    final: prev:
+
+    let
+      nixComponents = final.nixVersions.nixComponents_2_30;
+    in
+
+    {
+      nixd = prev.nixd.override (prev': {
+        inherit nixComponents;
+        nixt = prev'.nixt.override { inherit nixComponents; };
+      });
+    }
+  )
 ]
