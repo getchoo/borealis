fe0c09f377fa1b8a24359b0778e7a235b49a5e14
Seth Flynn <getchoo@tuta.io>
2025-09-19 21:50:27 -0400

    treewide: add my lix patches

--- flake/default.nix
+++ flake/default.nix
@@ -1,7 +1,23 @@
+{ inputs, self, ... }:
+
 {
   imports = [
     ./ci.nix
     ./dev-shells.nix
+    ./lix.nix
     ./treefmt.nix
   ];
+
+  perSystem =
+    { system, ... }:
+
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          inputs.lix-module.overlays.default
+          self.overlays.lix-patches
+        ];
+      };
+    };
 }
--- modules/shared/mixins/lix.nix
+++ modules/shared/mixins/lix.nix
@@ -1,8 +1,15 @@
-{ lib, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 
 {
   imports = [ inputs.lix-module.nixosModules.default ];
 
   # I prefer this to be opt-in for my systems
   lix.enable = lib.mkDefault false;
+
+  nixpkgs.overlays = lib.mkIf config.lix.enable [ inputs.self.overlays.lix-patches ];
 }
