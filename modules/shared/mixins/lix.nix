f5103d6b8f33986ac7fcdd3b8625a7f12fe35614
Seth Flynn <getchoo@tuta.io>
2025-09-21 04:28:21 -0400

    flake: remove lix patches

--- modules/shared/mixins/lix.nix
+++ modules/shared/mixins/lix.nix
@@ -1,15 +1,8 @@
-{
-  config,
-  lib,
-  inputs,
-  ...
-}:
+{ lib, inputs, ... }:
 
 {
   imports = [ inputs.lix-module.nixosModules.default ];
 
   # I prefer this to be opt-in for my systems
   lix.enable = lib.mkDefault false;
-
-  nixpkgs.overlays = lib.mkIf config.lix.enable [ inputs.self.overlays.lix-patches ];
 }
