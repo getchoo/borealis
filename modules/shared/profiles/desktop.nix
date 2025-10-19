5956ed5311c37505b231479a1a17f075cb1a48cb
Seth Flynn <getchoo@tuta.io>
2025-10-19 00:36:50 -0400

    modules/desktop: nix -> lix

--- modules/shared/profiles/desktop.nix
+++ modules/shared/profiles/desktop.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  inputs,
   ...
 }:
 
@@ -21,6 +20,8 @@ in
       };
     };
 
+    lix.enable = true;
+
     nix.settings = {
       extra-trusted-substituters = [
         "https://nix-community.cachix.org"
@@ -30,21 +31,6 @@ in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-
-    nixpkgs.overlays = [
-      inputs.nix.overlays.default
-
-      # TODO: Remove this!
-      # Un-vendor toml11 to fix build failures with the Nix flake's override
-      # https://github.com/NixOS/nix/pull/14199
-      (final: prev: {
-        nix = prev.nix.overrideScope (
-          _: _: {
-            inherit (final) toml11;
-          }
-        );
-      })
-    ];
 
     services = {
       tailscale.enable = lib.mkDefault true;
