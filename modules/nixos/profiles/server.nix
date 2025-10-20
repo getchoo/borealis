badd925da68abbab40228ed8846f128db2f2b260
Seth Flynn <getchoo@tuta.io>
2025-10-20 05:18:27 -0400

    nixos/server: cleanup nesting

--- modules/nixos/profiles/server.nix
+++ modules/nixos/profiles/server.nix
@@ -26,85 +26,82 @@ in
     enable = lib.mkEnableOption "the Server profile";
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      {
-        _module.args = {
-          # All servers are most likely on stable, so we want to pull in some newer packages from time to time
-          unstable = inputs'.nixpkgs.legacyPackages;
+  config = lib.mkIf cfg.enable {
+    _module.args = {
+      # All servers are most likely on stable, so we want to pull in some newer packages from time to time
+      unstable = inputs'.nixpkgs.legacyPackages;
 
-          secretsDir = inputs.self + "/secrets/${config.networking.hostName}";
-        };
+      secretsDir = inputs.self + "/secrets/${config.networking.hostName}";
+    };
 
-        age.secrets = {
-          discordWebhookURL.file = "${secretsDir}/discordWebhookURL.age";
-          tailscaleAuthKey.file = "${secretsDir}/tailscaleAuthKey.age";
-        };
+    age.secrets = {
+      discordWebhookURL.file = "${secretsDir}/discordWebhookURL.age";
+      tailscaleAuthKey.file = "${secretsDir}/tailscaleAuthKey.age";
+    };
 
-        boot.tmp.cleanOnBoot = lib.mkDefault true;
+    boot.tmp.cleanOnBoot = lib.mkDefault true;
 
-        borealis.users = {
-          system.enable = true;
-        };
+    borealis.users = {
+      system.enable = true;
+    };
 
-        # We don't need it here
-        documentation.enable = false;
+    # We don't need it here
+    documentation.enable = false;
 
-        environment = {
-          defaultPackages = lib.mkForce [ ];
-          systemPackages = with pkgs; [
-            ghostty.terminfo
-            rsync
-          ];
-        };
+    environment = {
+      defaultPackages = lib.mkForce [ ];
+      systemPackages = with pkgs; [
+        ghostty.terminfo
+        rsync
+      ];
+    };
 
-        nix.gc = {
-          dates = "*:0/30"; # Every 30 minutes
-          # GC to stay above minimumStorageBytes
-          options = toString [
-            "--delete-older-than 5d"
-            "--max-freed \"$((${toString minimumStorageKb} - 1024 * $(df -k --output=avail /nix/store | tail -n 1)))\""
-          ];
-        };
+    nix = {
+      gc = {
+        dates = "*:0/30"; # Every 30 minutes
+        # GC to stay above minimumStorageBytes
+        options = toString [
+          "--delete-older-than 5d"
+          "--max-freed \"$((${toString minimumStorageKb} - 1024 * $(df -k --output=avail /nix/store | tail -n 1)))\""
+        ];
+      };
+      # Hardening access to `nix` as no other users *should* ever really touch it
+      settings.allowed-users = lib.mkIf config.borealis.users.system.enable [
+        config.networking.hostName
+      ];
+    };
 
-        services = {
-          systemd-discord-notifier = {
-            enable = true;
+    services = {
+      systemd-discord-notifier = {
+        enable = true;
 
-            content = ''
-              # 🚨 %i.service failed! 🚨
-              <@746501080409702461>
-            '';
+        content = ''
+          # 🚨 %i.service failed! 🚨
+          <@746501080409702461>
+        '';
 
-            webhookURLFile = config.age.secrets.discordWebhookURL.path;
-          };
+        webhookURLFile = config.age.secrets.discordWebhookURL.path;
+      };
 
-          tailscale = {
-            enable = true;
+      tailscale = {
+        enable = true;
 
-            authKeyFile = config.age.secrets.tailscaleAuthKey.path;
-            extraUpFlags = [ "--ssh" ];
-          };
-        };
+        authKeyFile = config.age.secrets.tailscaleAuthKey.path;
+        extraUpFlags = [ "--ssh" ];
+      };
+    };
 
-        system.autoUpgrade = {
-          enable = true;
+    system.autoUpgrade = {
+      enable = true;
 
-          dates = "minutely";
-          flake = "github:getchoo/borealis#${config.networking.hostName}";
-          flags = [ "--refresh" ];
-        };
+      dates = "minutely";
+      flake = "github:getchoo/borealis";
+      flags = [ "--refresh" ];
+    };
 
-        # I use exclusively Tailscale auth on some machines
-        users.allowNoPasswordLogin = true;
+    # I use exclusively Tailscale auth on some machines
+    users.allowNoPasswordLogin = true;
 
-        zramSwap.enable = true;
-      }
-
-      (lib.mkIf config.borealis.users.system.enable {
-        # Hardening access to `nix` as no other users *should* ever really touch it
-        nix.settings.allowed-users = [ config.networking.hostName ];
-      })
-    ]
-  );
+    zramSwap.enable = true;
+  };
 }
