30ab7e89981c1d8949b25fe552c6b33a97404dfe
Seth Flynn <getchoo@tuta.io>
2026-02-17 16:29:43 -0500

    seth/shell: use moor for pager

--- users/seth/shell.nix
+++ users/seth/shell.nix
@@ -1,10 +1,18 @@
-{ config, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   home = {
     sessionVariables = {
       EDITOR = "nvim";
+      PAGER = lib.getExe pkgs.moor;
       VISUAL = config.home.sessionVariables.EDITOR;
+
+      # Force XDG spec on some apps
       CARGO_HOME = "${config.xdg.dataHome}/cargo";
       LESSHISTFILE = "${config.xdg.stateHome}/less/history";
     };
