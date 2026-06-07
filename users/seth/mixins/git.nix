99ceb6c45a8c24e1273e7fcb27edeb05faf550e1
Seth Flynn <getchoo@tuta.io>
2026-06-07 18:50:23 -0400

    seth/git: sign with 1p ssh

--- users/seth/mixins/git.nix
+++ users/seth/mixins/git.nix
@@ -1,4 +1,15 @@
 {
+  osConfig,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  isWsl = osConfig.wsl.enable or false;
+in
+
+{
   programs = {
     git = {
       settings = {
@@ -13,8 +24,10 @@
       };
 
       signing = {
-        key = "D31BD0D494BBEE86";
+        format = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBzp61t3KlcZZorMGzCHJJmiCoAEJsdHgYww80LmwkPd";
         signByDefault = true;
+        signer = if isWsl then "op-ssh-sign-wsl.exe" else lib.getExe' pkgs._1password-gui "op-ssh-sign";
       };
     };
 
