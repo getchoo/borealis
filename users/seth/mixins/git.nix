b96b594ec9d1ac08d519bd5d2f52039acc27d964
Seth Flynn <getchoo@tuta.io>
2026-08-16 17:47:09 -0400

    revert: "seth/git: sign with 1p ssh"
    This reverts commit 99ceb6c45a8c24e1273e7fcb27edeb05faf550e1.

--- users/seth/mixins/git.nix
+++ users/seth/mixins/git.nix
@@ -1,15 +1,4 @@
 {
-  osConfig,
-  lib,
-  pkgs,
-  ...
-}:
-
-let
-  isWsl = osConfig.wsl.enable or false;
-in
-
-{
   programs = {
     git = {
       settings = {
@@ -24,10 +13,8 @@ in
       };
 
       signing = {
-        format = "ssh";
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBzp61t3KlcZZorMGzCHJJmiCoAEJsdHgYww80LmwkPd";
+        key = "D31BD0D494BBEE86";
         signByDefault = true;
-        signer = if isWsl then "op-ssh-sign-wsl.exe" else lib.getExe' pkgs._1password-gui "op-ssh-sign";
       };
     };
 
